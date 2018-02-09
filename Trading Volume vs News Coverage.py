dc0a9c4f9846484e821521181572adc7b148b962
Li Na <lina@songyundeMacBook-Air.local>
2018-02-09 08:34:32 +0800

    “updated”

--- Trading Volume vs News Coverage.py
+++ Trading Volume vs News Coverage.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 from newsapi import NewsApiClient
 import numpy as np
 
-AnalysisStartDate = datetime.date(2018, 1, 26)
-NUM_OF_DAYS = 30
+AnalysisStartDate = datetime.date(2018, 2, 2)
+NUM_OF_DAYS = 4
 NEWS_API_KEY = 'ad490ddac082441d8f6980842b815629'
 ALPHA_API_KEY = "504P0GG4DGGV7D16"
-FUNC = "OBV"
+FUNC = "TIME_SERIES_DAILY"
 obvURL = "https://www.alphavantage.co/query?function=" + FUNC + "&interval=daily&apikey=" + ALPHA_API_KEY
 newsapi = NewsApiClient(api_key=NEWS_API_KEY)
 
@@ -36,32 +36,26 @@ def parseOBV(obvs, startDate):
 
 def getOBVFor(stockSymbol, url, analysisStartDate):
     url += "&symbol=" + stockSymbol
-    pp = getJsonResponse(url)
-    result = getJsonResponse(url)["Technical Analysis: OBV"]
+    result = getJsonResponse(url)["Time Series (Daily)"]
     obvResults = {}
     for weekDay, obvValue in result.iteritems():
-        obvResults[weekDay] = obvValue["OBV"]
-
+        obvResults[weekDay] = obvValue["5. volume"]
     return parseOBV(obvResults, analysisStartDate)
-
 
 def getNumberOfNews(searchTerm, startDate):
     prevWeekDay = previousWeekday(startDate)
-    #print "prev weekday: " + str(prevWeekDay)
-    #print "startDate: " + str(startDate)
     numberOfNews = newsapi.get_everything(q=searchTerm,
                                       from_parameter=str(prevWeekDay),
                                       to=str(startDate),
                                       language='en')
-    #print "count:" + str(numberOfNews["totalResults"])
     return numberOfNews["totalResults"]
 
 
-def getMonthlyNewsCount(company, startDate):
+def getMonthlyNewsCount(searchTerm, startDate):
     newsCount = {}
     for i in range(NUM_OF_DAYS):
         currentDate = str(startDate)
-        newsCount[startDate] = getNumberOfNews(company, startDate)
+        newsCount[startDate] = getNumberOfNews(searchTerm, startDate)
         startDate = previousWeekday(startDate)
     return newsCount
 
@@ -69,8 +63,8 @@ def getMonthlyNewsCount(company, startDate):
 def two_scales(ax1, dates, obvs, count):
     ax2 = ax1.twinx()
     ax1.plot(dates, obvs, '-bo')
-    ax1.set_xlabel('Dates')
-    ax1.set_ylabel('OBV')
+    ax1.set_xlabel('Date')
+    ax1.set_ylabel('Trading Volume')
 
     ax2.plot(dates, count, '-ro')
     ax2.set_ylabel('News Count')
@@ -84,7 +78,7 @@ def color_y_axis(ax, color):
 
 
 if __name__ == '__main__':
-    companyAndSymbols = { "Apple" : "AAPL", "Microsoft" : "MSFT"}
+    companyAndSymbols = { "Apple" : "AAPL"}
     for company, stockSymbol in companyAndSymbols.iteritems():
         obvresult = getOBVFor(stockSymbol, obvURL, AnalysisStartDate)
         monthlyNewsCount = getMonthlyNewsCount(company, AnalysisStartDate)
@@ -94,13 +88,16 @@ if __name__ == '__main__':
         for date in dates:
             obvs.append(obvresult[date])
             count.append(monthlyNewsCount[date])
-
-        # Plot for individual company
-        # fig, ax = plt.subplots()
-        # ax1, ax2 = two_scales(ax, dates, obvs, count)
-        # color_y_axis(ax1, 'b')
-        # color_y_axis(ax2, 'r')
-        # plt.show()
+            print "Current Week date: " + str(date)
+            print "==========================================="
+            print "OBV values: " + str(obvresult[date])
+            print "News Count: " + str(monthlyNewsCount[date])
         correlation = np.corrcoef(obvs, count)[0, 1]
         print "Company Name: " + company
         print "Correlation: " + str(correlation)
+        #Plot for individual company
+        fig, ax = plt.subplots()
+        ax1, ax2 = two_scales(ax, dates, obvs, count)
+        color_y_axis(ax1, 'b')
+        color_y_axis(ax2, 'r')
+        plt.show()
